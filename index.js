68e43f1a9bf7ee742164b9773594489c07f797d9
Manujdixit <94791934+Manujdixit@users.noreply.github.com>
2023-12-06 18:59:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("welcome");
+  return res.status(200).send("welcome");
 });
 
 app.use("/notes", booksRoute);
